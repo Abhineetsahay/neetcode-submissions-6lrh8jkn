351754bed342adaaddf2d366cf0359dae382bbdd
Abhineet Sahay <145606273+Abhineetsahay@users.noreply.github.com>
2026-04-27 19:44:37 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+   public:
+    stack<int> st;
+    stack<int> min_st;
+    MinStack() {}
+
+    void push(int val) {
+        st.push(val);
+        if (min_st.empty() || val <= min_st.top()) {
+            min_st.push(val);
+        }
+    }
+
+    void pop() {
+        if (min_st.top() == st.top()) min_st.pop();
+        st.pop();
+    }
+
+    int top() { return st.top(); }
+
+    int getMin() { return min_st.top(); }
+};
+
