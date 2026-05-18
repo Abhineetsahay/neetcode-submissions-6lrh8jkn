8ce4977cbe175a4dbf84a3bf085131142325a983
Abhineet Sahay <145606273+Abhineetsahay@users.noreply.github.com>
2026-05-19 01:15:46 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   private:
+    vector<vector<int>> dp;
+    int f(int ind, int prev_ind, vector<int>& nums) {
+        if (ind == nums.size()) {
+            return 0;
+        }
+        if (dp[ind][prev_ind + 1] != -1) {
+            return dp[ind][prev_ind + 1];
+        }
+
+        int len = f(ind + 1, prev_ind, nums);
+
+        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) {
+            len = max(len, 1 + f(ind + 1, ind, nums));
+        }
+        return dp[ind][prev_ind + 1] = len;
+    }
+
+   public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return f(0, -1, nums);
+    }
+};
