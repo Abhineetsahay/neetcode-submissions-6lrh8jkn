90d03bb28cb0a6615d5f8b427a5d77c637a8b68e
Abhineet Sahay <145606273+Abhineetsahay@users.noreply.github.com>
2026-05-21 17:18:51 +0530

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+   private:
+    vector<vector<int>> dp;
+    // bool f(int i, int j, string s1, string s2, string s3) {
+    //     if (i == s1.size() && j == s2.size()) return true;
+
+    //     if (dp[i][j] != -1) return dp[i][j];
+
+    //     int k = i + j;
+    //     bool takeS1 = false;
+    //     bool takeS2 = false;
+
+    //     if (i < s1.size() && s1[i] == s3[k]) {
+    //         takeS1 = f(i + 1, j, s1, s2, s3);
+    //     }
+
+    //     if (j < s2.size() && s2[j] == s3[k]) {
+    //         takeS2 = f(i, j + 1, s1, s2, s3);
+    //     }
+
+    //     return dp[i][j] = (takeS1 || takeS2);
+    // }
+
+   public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size();
+        int m = s2.size();
+
+        if (n + m != s3.size()) return false;
+
+        dp.assign(n + 1, vector<int>(m + 1, false));
+        dp[n][m] = true;
+        for (int i = n; i >= 0; i--) {
+            for (int j = m; j >= 0; j--) {
+                int k = i + j;
+                if (i < n && s1[i] == s3[k] && dp[i + 1][j]) {
+                    dp[i][j] = true;
+                }
+
+                if (j < m && s2[j] == s3[k] && dp[i][j + 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
