0e7b6cf010dc69d5a30aaa18a488604735a5cf0d
Abhineet Sahay <145606273+Abhineetsahay@users.noreply.github.com>
2026-05-31 17:32:57 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        for (auto& num : nums) {
+            if (num == 0) {
+                count0++;
+            } else if (num == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        for (int i = 0; i < count0; i++) {
+            nums[i] = 0;
+        }
+        for (int i = count0; i < count0 + count1; i++) {
+            nums[i] = 1;
+        }
+        for (int i = count0 + count1; i < n; i++) {
+            nums[i] = 2;
+        }
+    }
+};
