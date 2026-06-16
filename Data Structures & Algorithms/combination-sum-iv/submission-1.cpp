9c50731540c7da0a293ccf3dd498a5ce94037576
Abhineet Sahay <145606273+Abhineetsahay@users.noreply.github.com>
2026-06-16 18:35:36 +0530

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    vector<int> dp;
+
+   public:
+    int combinationSum4(vector<int>& nums, int target) {
+        dp.resize(target + 1);
+        dp[0] = 1;
+
+        for (int i = 0; i <= target; i++) {
+            for (int num : nums) {
+                if (i >= num) dp[i] += dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+};
